a332254d29317d249c5dc65b2ca4e45ded712725
santiago cortes <jeissoncortes02@outlook.com>
2021-08-05 08:59:03 -0500

    entidad y configuracion inicial

---
+++ src/main/java/inicio/model/Contacto.java
@@ -0,0 +1,56 @@
+package inicio.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="contactos")
+public class Contacto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int idContacto;
+
+    private int edad;
+
+    private String email;
+
+    private String nombre;
+
+    public Contacto() {
+    }
+
+    public int getIdContacto() {
+        return this.idContacto;
+    }
+
+    public void setIdContacto(int idContacto) {
+        this.idContacto = idContacto;
+    }
+
+    public int getEdad() {
+        return this.edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+}
